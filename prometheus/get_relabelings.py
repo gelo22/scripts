cc33889ad9f858982cb1a91d1a1305dce45b6d40
gelo <alexey.kurinnij@gmail.com>
2021-11-01 13:52:22 +0200

    python2 compat added

--- prometheus/get_relabelings.py
+++ prometheus/get_relabelings.py
@@ -1,4 +1,10 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python
+
+# python3 compat code
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
 import re
 import sys
